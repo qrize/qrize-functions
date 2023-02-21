65f41473fe826f26d0b7efb3a65041cc0b6c3e08
Serhii Holinei <sergey@goliney.com>
2023-02-21 00:50:53 -0500

    test

--- packages/f/r/__main__.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def main(args):
-      name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!"
-      if name == 'redirect':
-            return { "statusCode": 301, "headers": { "location": "https://example.com"}}
-      return {"body": json.dumps(args)}
-  
---
+++ packages/p/r/__main__.py
@@ -0,0 +1,23 @@
+def main(args):
+      path = args.http.path # e.g. "/some-hash"
+      hash = path[1:] # remove first symbol (slash '/')
+
+      # get URL by hash
+      # TODO
+      url = hash
+
+      if url == 'empty':
+            return {
+                  "statusCode": 404
+            }
+
+      if url == 'redirect':
+            return {
+                  "statusCode": 301,
+                  "headers": {
+                        "location": "https://example.com"
+                  }
+            }
+
+      return { "body": url }
+  
