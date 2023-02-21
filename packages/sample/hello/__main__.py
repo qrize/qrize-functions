ba5bcb0c6fbe1fd3e6f0828c61389bf7a8d1ea7d
Serhii Holinei <sergey@goliney.com>
2023-02-21 00:30:17 -0500

    test 10

--- packages/sample/hello/__main__.py
+++
@@ -1,8 +0,0 @@
-def main(args):
-      name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!"
-      if name == 'redirect':
-            return { "statusCode": 301, "headers": { "location": "https://example.com"}}
-      print(greeting)
-      return {"body": greeting}
-  
