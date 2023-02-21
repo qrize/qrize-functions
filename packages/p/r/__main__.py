67f4e6736258faf4a012bc1a583ff196a52c2dfc
Serhii Holinei <sergey@goliney.com>
2023-02-21 01:37:24 -0500

    test

--- packages/p/r/__main__.py
+++ packages/p/r/__main__.py
@@ -1,24 +1,23 @@
 def main(args):
-      return { "body": "path is: '%s'" % args.get('http')['path'] }
-      # path = args.get('http')['path'] # e.g. "/some-hash"
-      # hash = path[1:] # remove first symbol (slash '/')
+      path = args.get('http')['path'] # e.g. "/some-hash"
+      hash = path[1:] # remove first symbol (slash '/')
 
-      # # get URL by hash
-      # # TODO
-      # url = hash
+      # get URL by hash
+      # TODO
+      url = hash
 
-      # if url == 'empty':
-      #       return {
-      #             "statusCode": 404
-      #       }
+      if url == 'empty':
+            return {
+                  "statusCode": 404
+            }
 
-      # if url == 'redirect':
-      #       return {
-      #             "statusCode": 301,
-      #             "headers": {
-      #                   "location": "https://example.com"
-      #             }
-      #       }
+      if url == 'redirect':
+            return {
+                  "statusCode": 301,
+                  "headers": {
+                        "location": "https://example.com"
+                  }
+            }
 
-      # return { "body": url }
+      return { "body": "hash is %s" % hash }
   
